840a05e3e50fd9373cf2a3d8a7eb227aa7f8d28e
Virjilakrum <asesenep15@gmail.com>
2024-05-29 14:48:17 +0300

    add generate nft scripts

---
+++ frontend/src/components/NFTCreation.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { userStore, imageStore } from "../stores";
+
+    $: user = $userStore;
+    $: imageUrl = $imageStore;
+
+    async function createNFT() {
+        await fetch("http://localhost:3000/nft", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${user.token}`,
+            },
+            body: JSON.stringify({ image: imageUrl }),
+        });
+    }
+</script>
+
+<main>
+    <button on:click={createNFT}>Generate NFT</button>
+</main>
